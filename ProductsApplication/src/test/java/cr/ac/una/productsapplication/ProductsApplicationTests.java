d04b769261bba8564d6433eab54e95f70e49e019
Elías Mora <ls.elias.mora@gmail.com>
2026-03-09 17:45:27 -0600

    initialize project structure with Maven and Spring Boot setup

---
+++ ProductsApplication/src/test/java/cr/ac/una/productsapplication/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.productsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
